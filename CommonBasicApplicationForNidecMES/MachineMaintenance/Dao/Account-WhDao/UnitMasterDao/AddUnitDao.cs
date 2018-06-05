a262b372cfdf6eec9d47527918daac90746addc5
Long <mt-fa29@nidec-copal.vn>
2018-06-05 09:11:28 +0700

    up

---
+++ CommonBasicApplicationForNidecMES/MachineMaintenance/Dao/Account-WhDao/UnitMasterDao/AddUnitDao.cs
@@ -0,0 +1,42 @@
+﻿using Com.Nidec.Mes.Common.Basic.MachineMaintenance.Vo;
+using Com.Nidec.Mes.Framework;
+using Com.Nidec.Mes.GlobalMasterMaintenance.Vo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Dao
+{
+    public class AddUnitDao : AbstractDataAccessObject
+    {
+        public override ValueObject Execute(TransactionContext trxContext, ValueObject vo)
+        {
+            UnitVo inVo = (UnitVo)vo;
+            StringBuilder sql = new StringBuilder();
+            sql.Append("insert into m_unit(unit_cd, unit_name, registration_user_cd, registration_date_time, factory_cd) ");
+            sql.Append("values(:unit_cd,:unit_name, :registration_user_cd,now(),:factory_cd)");
+            
+            //create command
+            DbCommandAdaptor sqlCommandAdapter = base.GetDbCommandAdaptor(trxContext, sql.ToString());
+
+            //create parameter
+            DbParameterList sqlParameter = sqlCommandAdapter.CreateParameterList();
+            sqlParameter.AddParameterString("unit_cd", inVo.UnitCode);
+            sqlParameter.AddParameterString("unit_name", inVo.UnitName);
+        
+            sqlParameter.AddParameterString("factory_cd", inVo.FactoryCode);
+            sqlParameter.AddParameterString("registration_user_cd", inVo.RegistrationUserCode);
+            //execute SQL
+
+            UnitVo outVo = new UnitVo
+            {
+                AffectedCount = sqlCommandAdapter.ExecuteNonQuery(sqlParameter)
+            };
+
+            return outVo;
+        }
+    }
+}
