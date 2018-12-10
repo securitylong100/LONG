5d1eb887e14c24e0bb6ea0de75e2e6dbc59721dd
quangdang2012 <minhhieuyahoo7776@gmail.com>
2018-12-10 17:04:40 +0700

    up new NCVC

---
+++ CommonBasicApplicationForNidecMES/MachineMaintenance/Dao/ProductionControllerNCVCDao/GetLineProNCVCDao.cs
@@ -0,0 +1,50 @@
+﻿using Com.Nidec.Mes.Common.Basic.MachineMaintenance.Vo;
+using Com.Nidec.Mes.Framework;
+using Com.Nidec.Mes.GlobalMasterMaintenance.Vo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Dao
+{
+    public class GetLineProNCVCDao : AbstractDataAccessObject
+    {
+        public override ValueObject Execute(TransactionContext trxContext, ValueObject vo)
+        {
+            ProductionControllerNCVCVo inVo = (ProductionControllerNCVCVo)vo;
+            StringBuilder sql = new StringBuilder();
+            ValueObjectList<ProductionControllerNCVCVo> voList = new ValueObjectList<ProductionControllerNCVCVo>();
+            //create command
+            DbCommandAdaptor sqlCommandAdapter = base.GetDbCommandAdaptor(trxContext, sql.ToString());
+
+            //create parameter
+            
+            DbParameterList sqlParameter = sqlCommandAdapter.CreateParameterList();
+            sql.Append(@"select distinct  line_cd from t_ncvp_pdc_fc union select 'All Line' ");
+            sql.Append("order by line_cd");
+
+            sqlCommandAdapter = base.GetDbCommandAdaptor(trxContext, sql.ToString());
+            
+
+            //execute SQL
+            IDataReader dataReader = sqlCommandAdapter.ExecuteReader(trxContext, sqlParameter);
+
+            while (dataReader.Read())
+            {
+                ProductionControllerNCVCVo outVo = new ProductionControllerNCVCVo
+                {
+
+             
+                    ProLine = dataReader["line_cd"].ToString(),
+                    
+                };
+                voList.add(outVo);
+            }
+            dataReader.Close();
+            return voList;
+        }
+    }
+}
---
+++ CommonBasicApplicationForNidecMES/MachineMaintenance/Dao/ProductionControllerNCVCDao/GetModelProNCVCDao.cs
@@ -0,0 +1,44 @@
+﻿using Com.Nidec.Mes.Common.Basic.MachineMaintenance.Vo;
+using Com.Nidec.Mes.Framework;
+using Com.Nidec.Mes.GlobalMasterMaintenance.Vo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Dao
+{
+    public class GetModelProNCVCDao : AbstractDataAccessObject
+    {
+        public override ValueObject Execute(TransactionContext trxContext, ValueObject vo)
+        {
+            ProductionControllerNCVCVo inVo = (ProductionControllerNCVCVo)vo;
+            StringBuilder sql = new StringBuilder();
+            ValueObjectList<ProductionControllerNCVCVo> voList = new ValueObjectList<ProductionControllerNCVCVo>();
+            //create command
+            DbCommandAdaptor sqlCommandAdapter = base.GetDbCommandAdaptor(trxContext, sql.ToString());
+
+            //create parameter
+            DbParameterList sqlParameter = sqlCommandAdapter.CreateParameterList();
+            sql.Append(@"select distinct  model_cd from t_ncvp_pdc_fc order by model_cd ");
+
+            sqlCommandAdapter = base.GetDbCommandAdaptor(trxContext, sql.ToString());
+
+            //execute SQL
+            IDataReader dataReader = sqlCommandAdapter.ExecuteReader(trxContext, sqlParameter);
+
+            while (dataReader.Read())
+            {
+                ProductionControllerNCVCVo outVo = new ProductionControllerNCVCVo
+                {             
+                    ProModel = dataReader["model_cd"].ToString(),                    
+                };
+                voList.add(outVo);
+            }
+            dataReader.Close();
+            return voList;
+        }
+    }
+}
