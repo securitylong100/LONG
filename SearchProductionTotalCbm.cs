11d105cb1cb784effeebd7e2c2979ce0d9a4b99b
quangdang2012 <30891419+quangdang2012@users.noreply.github.com>
2019-01-11 08:17:03 +0700

    up
    up

---
+++ SearchProductionTotalCbm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Nidec.Mes.Common.Basic.MachineMaintenance.Dao;
+using Com.Nidec.Mes.Common.Basic.MachineMaintenance.Vo;
+using Com.Nidec.Mes.Framework;
+
+
+namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Cbm
+{
+    public class SearchProductionTotalCbm : CbmController
+    {
+        private static readonly DataAccessObject getDao = new SearchProductionTotalDao();
+        public ValueObject Execute(TransactionContext trxContext, ValueObject vo)
+        {
+            if (vo == null)
+            {
+                return null;
+            }
+
+            return getDao.Execute(trxContext, vo);
+        }
+    }
+
+
+
+
+
+}
