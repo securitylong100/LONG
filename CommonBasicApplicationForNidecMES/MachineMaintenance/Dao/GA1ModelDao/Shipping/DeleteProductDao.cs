b1795b3b5d4ecb54eba4c9b23910888fa44bb568
securitylong100 <30890603+securitylong100@users.noreply.github.com>
2019-02-12 08:30:56 +0700

    Add files via upload

---
+++ CommonBasicApplicationForNidecMES/MachineMaintenance/Dao/GA1ModelDao/Shipping/DeleteProductDao.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using Com.Nidec.Mes.Framework;
+using Com.Nidec.Mes.GlobalMasterMaintenance.Vo;
+using System.Data;
+using System;
+using Com.Nidec.Mes.Common.Basic.MachineMaintenance.Vo;
+
+namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Dao
+{
+    class DeleteProductDao : AbstractDataAccessObject
+    {
+        public override ValueObject Execute(TransactionContext trxContext, ValueObject vo)
+        {
+            GA1ModelVo inVo = (GA1ModelVo)vo;
+            StringBuilder sql = new StringBuilder();
+            GA1ModelVo voList = new GA1ModelVo();
+            //create command
+            DbCommandAdaptor sqlCommandAdapter = base.GetDbCommandAdaptor(trxContext, sql.ToString());
+
+            //create parameter
+            DbParameterList sqlParameter = sqlCommandAdapter.CreateParameterList();
+            sql.Append("delete from t_product_serial where boxid = :boxid");
+            sqlParameter.AddParameter("boxid", inVo.BoxID);
+
+            sqlCommandAdapter = base.GetDbCommandAdaptor(trxContext, sql.ToString());
+            DataSet ds = new DataSet();
+            ds = sqlCommandAdapter.ExecuteDataSet(sqlParameter);
+            //execute SQL
+            //IDataReader dataReader = sqlCommandAdapter.ExecuteReader(trxContext, sqlParameter);
+
+            GA1ModelVo outVo = new GA1ModelVo
+            {
+                AffectedCount = sqlCommandAdapter.ExecuteNonQuery(sqlParameter)
+            };
+
+            return outVo;
+        }
+    }
+}
